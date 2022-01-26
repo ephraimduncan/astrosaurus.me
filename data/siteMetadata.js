864ad2833da836675bd880bd990e09fab4c2a57a
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-01-26 22:11:11 +0000

    chore: change twitter card preview

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,7 +8,7 @@ const siteMetadata = {
   siteUrl: 'https://astrosaurus.me',
   siteRepo: 'https://github.com/dephraiim/astrosaurus.me',
   siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
+  image: '/static/images/avatar.jpeg',
   socialBanner: '/static/images/twitter-card.png',
   email: 'ephraimduncan68@gmail.com',
   github: 'https://github.com/dephraiim',
