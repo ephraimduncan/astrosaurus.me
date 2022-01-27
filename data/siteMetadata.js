683a80f295b4b6c878a8699604c20b3c3e3d54be
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-01-27 10:57:27 +0000

    chore: add google analytics id

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -20,7 +20,7 @@ const siteMetadata = {
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-LEKDQZG4GD', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo
