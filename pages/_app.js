ff6c673a4f0b5212b335d3a782366ba6a270a5e3
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-06-02 01:49:24 +0000

    chore: place analytics in header tag

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,7 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <Analytics />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
