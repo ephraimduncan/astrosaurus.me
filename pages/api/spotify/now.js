1a50d90fd007e5ca49b97cf9561972c37fda8c05
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-06-30 17:49:08 +0000

    improvise spotify current playing endpoint for podcasts

--- pages/api/spotify/now.js
+++ pages/api/spotify/now.js
@@ -8,19 +8,31 @@ export default async (_, res) => {
   }
 
   const song = await response.json();
-  const isPlaying = song.is_playing;
-  const title = song.item.name;
-  const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
-  const album = song.item.album.name;
-  const albumImageUrl = song.item.album.images[0].url;
-  const songUrl = song.item.external_urls.spotify;
 
-  return res.status(200).json({
-    album,
-    albumImageUrl,
-    artist,
-    isPlaying,
-    songUrl,
-    title,
-  });
+  if (song.currently_playing_type === 'episode') {
+    return res.status(200).json({
+      isPlaying: true,
+      title: 'Listening to a Podcast',
+      artist: 'Spotify',
+      songUrl: 'https://open.spotify.com/show/2Shpxw7dPoxRJCdfFXTWLE',
+    });
+  } else {
+    const isPlaying = song.is_playing;
+    const title = song.item?.name;
+    const artist = song.item?.artists.map((_artist) => _artist.name).join(', ');
+    const album = song.item?.album.name;
+    const albumImageUrl = song.item?.album.images[0].url;
+    const songUrl = song.item?.external_urls.spotify;
+
+    console.log(song);
+
+    return res.status(200).json({
+      album,
+      albumImageUrl,
+      artist,
+      isPlaying,
+      songUrl,
+      title,
+    });
+  }
 };
