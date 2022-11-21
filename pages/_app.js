1f2abc8fc122cad1fb9b952fc4dd8c76560ce5ad
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-11-21 20:22:52 +0000

    feat(analytics): Add analytics with Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,9 @@ import '@/css/prism.css';
 
 import { ThemeProvider } from 'next-themes';
 import Head from 'next/head';
-import splitbee from '@splitbee/web';
+import { Analytics } from '@vercel/analytics/react';
 
 import siteMetadata from '@/data/siteMetadata';
-import Analytics from '@/components/analytics';
 import LayoutWrapper from '@/components/LayoutWrapper';
 import { ClientReload } from '@/components/ClientReload';
 
@@ -14,18 +13,15 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 const isSocket = process.env.SOCKET;
 
 export default function App({ Component, pageProps }) {
-  splitbee.init();
-
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <Analytics />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
+        <Analytics />
       </LayoutWrapper>
     </ThemeProvider>
   );
