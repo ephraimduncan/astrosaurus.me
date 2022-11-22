bd86cac08d7a1ae3f9118fd8d1c362a344daf4ee
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-11-22 22:39:16 +0000

    refactor(comment): Use Giscus component

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import GiscusComponent from '@giscus/react';
 import { useTheme } from 'next-themes';
 
 import siteMetadata from '@/data/siteMetadata';
-import Script from 'next/script';
 
 const Giscus = () => {
   const { theme, resolvedTheme } = useTheme();
@@ -12,8 +12,6 @@ const Giscus = () => {
         ? siteMetadata.comment.giscusConfig.darkTheme
         : siteMetadata.comment.giscusConfig.theme
       : siteMetadata.comment.giscusConfig.themeURL;
-
-  const COMMENTS_ID = 'comments-container';
 
   const {
     repo,
@@ -29,28 +27,20 @@ const Giscus = () => {
 
   return (
     <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300">
-      <div className="giscus" id={COMMENTS_ID}>
-        <Script
-          src="https://giscus.app/client.js"
-          data-repo={repo}
-          data-repo-id={repositoryId}
-          data-category={category}
-          data-category-id={categoryId}
-          data-mapping={mapping}
-          data-strict="0"
-          data-reactions-enabled={reactions}
-          data-emit-metadata={metadata}
-          data-input-position={inputPosition}
-          data-theme={commentsTheme}
-          data-lang={lang}
-          data-loading="lazy"
-          crossorigin="anonymous"
-          async
-          onLoad={() => {
-            console.log(siteMetadata?.comment?.giscusConfig);
-          }}
-        />
-      </div>
+      <GiscusComponent
+        id="comments"
+        repo={repo}
+        repoId={repositoryId}
+        category={category}
+        categoryId={categoryId}
+        mapping={mapping}
+        reactionsEnabled={reactions}
+        emitMetadata={metadata}
+        inputPosition={inputPosition}
+        theme={commentsTheme}
+        lang={lang}
+        loading="lazy"
+      />
     </div>
   );
 };
