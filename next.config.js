bb16023fd1f9e42564ddf95ee94b24e9710d0441
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-11-22 21:06:37 +0000

    chore(next-config): Update next.config.js to use Vercel Analytics

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.googletagmanager.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.googletagmanager.com *.vercel-insights.com;
   style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net;
   img-src * blob: data:;
   media-src 'none';
@@ -49,6 +49,10 @@ const securityHeaders = [
   {
     key: 'Permissions-Policy',
     value: 'camera=(), microphone=(), geolocation=()',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'interest-cohort=()',
   },
 ];
 
