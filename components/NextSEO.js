06b755c5ecc9b7b6418b412f2d73eaf850f6f244
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-11-28 21:52:40 +0000

    chore: update hostname to preview on test deployment urls

--- components/NextSEO.js
+++ components/NextSEO.js
@@ -1,39 +1,58 @@
+import React from 'react';
 import { NextSeo } from 'next-seo';
 
-export const MainSEO = () => (
-  <>
-    <NextSeo
-      title="Ephraim Atta-Duncan"
-      description="A Software Developer in Ghanna"
-      openGraph={{
-        type: 'website',
-        url: 'https://astrosaurus.me/',
-        title: 'Ephraim Atta-Duncan',
-        description: 'A Software Engineer in Ghana',
-        images: [{ url: 'https://astrosaurus.me/api/og' }],
-        siteName: 'Ephraim Atta-Duncan Personal Portfolio',
-      }}
-      twitter={{
-        handle: '@EphraimDuncan_',
-        site: '@EphraimDuncan_',
-        cardType: 'summary_large_image',
-      }}
-      robotsProps={{
-        nosnippet: true,
-        notranslate: true,
-        noimageindex: true,
-        noarchive: true,
-        maxSnippet: -1,
-        maxImagePreview: 'none',
-        maxVideoPreview: -1,
-      }}
-    />
-  </>
-);
+export const MainSEO = () => {
+  const [hostname, setHostname] = React.useState('');
+
+  React.useEffect(() => {
+    if (typeof window !== undefined) {
+      setHostname(window.location.hostname);
+    }
+  }, []);
+
+  return (
+    <>
+      <NextSeo
+        title="Ephraim Atta-Duncan"
+        description="A Software Developer in Ghanna"
+        openGraph={{
+          type: 'website',
+          url: hostname,
+          title: 'Ephraim Atta-Duncan',
+          description: 'A Software Engineer in Ghana',
+          images: [{ url: `${hostname}/api/og` }],
+          siteName: 'Ephraim Atta-Duncan Personal Portfolio',
+        }}
+        twitter={{
+          handle: '@EphraimDuncan_',
+          site: '@EphraimDuncan_',
+          cardType: 'summary_large_image',
+        }}
+        robotsProps={{
+          nosnippet: true,
+          notranslate: true,
+          noimageindex: true,
+          noarchive: true,
+          maxSnippet: -1,
+          maxImagePreview: 'none',
+          maxVideoPreview: -1,
+        }}
+      />
+    </>
+  );
+};
 
 export const BlogSEO = ({ authorDetails, title, summary, date, lastmod, url, tags }) => {
   const publishedAt = new Date(date).toISOString();
   const modifiedAt = new Date(lastmod || date).toISOString();
+
+  const [hostname, setHostname] = React.useState('');
+
+  React.useEffect(() => {
+    if (typeof window !== undefined) {
+      setHostname(window.location.hostname);
+    }
+  }, []);
 
   return (
     <>
@@ -48,12 +67,12 @@ export const BlogSEO = ({ authorDetails, title, summary, date, lastmod, url, tag
             modifiedTime: modifiedAt,
             section: title,
 
-            authors: ['Ephraim Atta-Duncan', 'https://astrosaurus.me/about'],
+            authors: ['Ephraim Atta-Duncan', 'https://astrosaurus.me/'],
             tags: tags,
           },
           images: [
             {
-              url: `https://astrosaurus.me/api/og?title=${title}`,
+              url: `${hostname}/api/og?title=${title}`,
               alt: title,
             },
           ],
