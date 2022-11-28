4d12dad9078e3e2d736e2fd6641ce42e6b555452
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-11-28 19:46:47 +0000

    feat(og-image): Initial OG Image Styling

--- pages/api/og.js
+++ pages/api/og.js
@@ -4,27 +4,84 @@ export const config = {
   runtime: 'experimental-edge',
 };
 
-export default function () {
+const font = fetch(new URL('../../public/fonts/Inter-Bold.ttf', import.meta.url)).then((res) =>
+  res.arrayBuffer()
+);
+
+export default async function () {
+  const fontData = await font;
+
   return new ImageResponse(
     (
       <div
         style={{
-          fontSize: 128,
-          background: 'white',
-          width: '100%',
-          height: '100%',
           display: 'flex',
-          textAlign: 'center',
-          alignItems: 'center',
-          justifyContent: 'center',
+          flexDirection: 'column',
+          height: '100%',
+          width: '100%',
+          justifyContent: 'space-between',
+          letterSpacing: '-.02em',
+          fontWeight: 700,
+          background: 'white',
+          fontFamily: 'Inter',
+          backgroundColor: '#000',
+          color: '#fff',
+          border: '6px solid white',
         }}
       >
-        Hello world!
+        <div
+          style={{
+            left: 80,
+            top: 80,
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <img
+            width="32"
+            height="32"
+            src="https://pbs.twimg.com/profile_images/1593380348485844996/ZsjjzAMD_400x400.jpg"
+            style={{
+              borderRadius: 128,
+            }}
+          />
+          <span
+            style={{
+              marginLeft: 8,
+              fontSize: 20,
+              fontSize: '32px',
+            }}
+          >
+            astrosaurus.me
+          </span>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'flex-start',
+            margin: '80px',
+            width: 'auto',
+            maxWidth: 600,
+            fontSize: 67,
+            fontWeight: 700,
+            lineHeight: '100%',
+            letterSpacing: '-4px',
+          }}
+        >
+          Making the Web. Faster.
+        </div>
       </div>
     ),
     {
       width: 1200,
-      height: 600,
+      height: 630,
+      fonts: [
+        {
+          name: 'Inter',
+          data: fontData,
+          style: 'bold',
+        },
+      ],
     }
   );
 }
