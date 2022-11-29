a949cd1b3b14713ba076cabe0a629d3248174e4b
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-11-29 00:26:16 +0000

    fix: hanging error

--- next.config.js
+++ next.config.js
@@ -66,23 +66,4 @@ module.exports = withBundleAnalyzer({
       },
     ];
   },
-
-  webpack: (config, { dev, isServer }) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-
-    return config;
-  },
 });
