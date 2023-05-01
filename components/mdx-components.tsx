86b35cb19c75945f1ffe763085e4ee62eb6ee83c
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-05-01 06:58:54 +0000

    Add syntax highlighting

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,16 +1,24 @@
-import Image from "next/image"
-import { useMDXComponent } from "next-contentlayer/hooks"
+import Image from "next/image";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import { Code } from "bright";
+
+Code.theme = {
+  dark: "github-dark",
+  light: "github-light",
+  lightSelector: "html.light",
+};
 
 const components = {
   Image,
-}
+  pre: Code,
+};
 
 interface MdxProps {
-  code: string
+  code: string;
 }
 
 export function Mdx({ code }: MdxProps) {
-  const Component = useMDXComponent(code)
+  const Component = useMDXComponent(code);
 
-  return <Component components={components} />
+  return <Component components={components} />;
 }
